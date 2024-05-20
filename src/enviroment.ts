7edc6105064291bf78ad1cad55dcba7e16111e95
Trong Quynh <nongtrongquynh050201@gmail.com>
2024-05-20 21:56:18 +0700

    Add stun server ip

--- src/enviroment.ts
+++ src/enviroment.ts
@@ -4,7 +4,10 @@ export const enviroment = {
             {
                 urls: [
                     'stun:stun.l.google.com:19302',
-                    'stun:stun1.l.google.com:19302'
+                    'stun:stun1.l.google.com:19302',
+                    'stun2.l.google.com:19302',
+                    'stun3.l.google.com:19302',
+                    'stun4.l.google.com:19302'
                 ]
             }
         ]
